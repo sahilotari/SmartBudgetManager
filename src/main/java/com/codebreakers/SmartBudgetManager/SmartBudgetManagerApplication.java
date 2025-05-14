eee01e4e92fc2108d402d2e46b87b0af8d9e2e6f
Sahil <sahilotari3@gmail.com>
2025-05-14 10:00:31 +0000

    Initilize project

---
+++ src/main/java/com/codebreakers/SmartBudgetManager/SmartBudgetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.codebreakers.SmartBudgetManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBudgetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBudgetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebreakers/SmartBudgetManager/SmartBudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebreakers.SmartBudgetManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
